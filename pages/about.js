3af8d6ada932e7c887ddfde0cefb20a37b12d80c
Alex Beattie <artisanbranding@me.com>
2024-06-12 22:08:34 -0700

    added a temporary home page

---
+++ pages/about.js
@@ -0,0 +1,85 @@
+// pages/about.js
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function About() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Head>
+        <title>About Us - Florida Luxury Collection - Luxury Properties</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <section className="py-20">
+          <div className="container mx-auto px-6">
+            <h2 className="text-4xl mb-8 font-secondary text-slate-500 text-center">
+              About FLC Luxury Collection
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              <div className="text-center">
+                <div className="bg-white p-6 shadow-lg rounded-lg">
+                  <div className="relative w-full h-0 pb-square mb-4">
+                    <Image
+                      src="/images/sheri-skora.jpg"
+                      alt="Agent 1"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute inset-0"
+                    />
+                  </div>
+                  <h3 className="text-xl font-bold text-slate-500">Sheri Skora</h3>
+                  <p className="text-slate-500"></p>
+                </div>
+              </div>
+              <div className="text-center">
+                <div className="bg-white p-6 shadow-lg rounded-lg">
+                  <div className="relative w-full h-0 pb-square mb-4">
+                    <Image
+                      src="/images/kristin-kuntz-leon.jpg"
+                      alt="Agent 2"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute inset-0"
+                    />
+                  </div>
+                  <h3 className="text-xl font-bold text-slate-500">Kristin Kuntz</h3>
+                  <p className="text-slate-500"></p>
+                </div>
+              </div>
+              <div className="text-center">
+                <div className="bg-white p-6 shadow-lg rounded-lg">
+                  <div className="relative w-full h-0 pb-square mb-4">
+                    <Image
+                      src="/images/connie-joe-redman.jpg"
+                      alt="Agent 3"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute inset-0"
+                    />
+                  </div>
+                  <h3 className="text-xl font-bold text-slate-500">Agent 3</h3>
+                  <p className="text-slate-500"></p>
+                </div>
+              </div>
+              <div className="text-center">
+                <div className="bg-white p-6 shadow-lg rounded-lg">
+                  <div className="relative w-full h-0 pb-square mb-4">
+                    <Image
+                      src="/images/kellie-mullen.jpg"
+                      alt="Agent 4"
+                      layout="fill"
+                      objectFit="cover"
+                      className="absolute inset-0"
+                    />
+                  </div>
+                  <h3 className="text-xl font-bold text-slate-500">Agent 4</h3>
+                  <p className="text-slate-500"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,45 @@
-// pages/services.js
-const Home = () => (
-  <div>
-    <h1 className="text-3xl font-bold">Home</h1>
-  </div>
-);
+// !--pages / index.js-- >
 
-export default Home;
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Head>
+        <title>Luxury Properties</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     
+
+     <main>
+        <section className="py-20">
+          <div className="container mx-auto px-6">
+            <h2 className="text-4xl mb-2 font-secondary text-center text-slate-500">Tampa Bay & St. Petersburg Beach</h2>
+            <h3 className="text-2xl text-center mb-8 text-gray-600">Welcome to Florida</h3>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div>
+                <Image src="/images/property2.jpg" alt="Property 1" width={500} height={300} className="rounded-lg shadow-lg" />
+                {/* <h4 className="text-xl font-bold mt-4">Elegant Villa</h4> */}
+                {/* <p className="text-gray-600">Spacious villa with lush gardens and a private pool.</p> */}
+              </div>
+              <div>
+                <Image src="/images/property3.jpg" alt="Property 2" width={500} height={300} className="rounded-lg shadow-lg" />
+                {/* <h4 className="text-xl font-bold mt-4">Waterfront Mansion</h4> */}
+                {/* <p className="text-gray-600">Stunning mansion with breathtaking ocean views.</p> */}
+              </div>
+              <div>
+                <Image src="/images/property1.jpg" alt="Property 3" width={500} height={300} className="rounded-lg shadow-lg" />
+                {/* <h4 className="text-xl font-bold mt-4">Modern Penthouse</h4> */}
+                {/* <p className="text-gray-600">Luxurious penthouse with floor-to-ceiling windows and city skyline views.</p> */}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+
+    </div>
+  );
+}
